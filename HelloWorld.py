31feca22290c1f50bbaef312905d82e1b32451db
Maximiliano Afonso <mecsafonso@gmail.com>
2020-04-10 16:50:50 -0300

    Math Operators

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+print ('Hello World!')
+
+# Math Operators from Highest to Lowest Precedence
+# Operator          Operation               Example         Evaluates to..
+#   **              Exponent                2 ** 3              8
+#   %               Modulus/remainder       22 % 8              6
+#   //              Integer division /
+#                   floored quotient        22 // 8             2
+#   /               Division                22 / 8              2.75
+#   *               Multiplication          3 * 5               15
+#   -               Subtraction             5 - 2               3
+#   +               Addition                2 + 2               4
+
+
+
+print ('Goodbye cruel world!')
