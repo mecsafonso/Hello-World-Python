b97409dba75763f6cc15f539a1d6f14a2934b9b6
Maximiliano Afonso <mecsafonso@gmail.com>
2020-04-11 08:52:57 -0300

    Importing Modules example

---
+++ printRandom.py
@@ -0,0 +1,7 @@
+#example for Importing Modules
+
+# Before you can use the functions in a module, you must import the module with an import statement.
+
+import random
+for i in range(5):
+    print(random.randint(1,10))
